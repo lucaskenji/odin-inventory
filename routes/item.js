b1931bf48424ac001f717273ac2960d7e5c44b80
Lucas <ra1957066@gmail.com>
2020-10-07 22:25:56 -0300

    Revert "Revert "Implemented image upload feature""
    This reverts commit 24456e60b15419589ba32ec2998cee8457bef97e.

--- routes/item.js
+++ routes/item.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/item.js');
-
+const multer = require('multer');
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './public/uploads')
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+		cb(null, uniqueSuffix + file.originalname);
+	}
+});
+const upload = multer({ storage: storage });
 
 // Routes
 router.get('/new', controller.getItemForm);
@@ -12,9 +22,9 @@ router.get('/delete/:url', controller.getDeletePrompt);
 
 router.get('/:url', controller.getItem);
 
-router.post('/new', controller.itemValidation, controller.addNewItem);
+router.post('/new', upload.single('photo'), controller.itemValidation, controller.addNewItem);
 
-router.post('/edit/:url', controller.itemValidation, controller.editItem);
+router.post('/edit/:url', upload.single('photo'), controller.itemValidation, controller.editItem);
 
 router.post('/delete/:url', controller.deleteItem);
 
